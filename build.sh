18683ebbd13fde4a5bba0b21f73a856033ae1fb1
jcgcecilia <jcgcecilia@gmail.com>
2016-07-16 17:03:44 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 #docker run -v ${PWD}:/home/QtDemo -i --entrypoint /bin/bash jcgcecilia/qt:5.7 -c 'cd /home/QtDemo;/usr/local/bin/qmake QtDemo.pro CONFIG+=agi;make -j4;make clean'
 ls
 echo ${PWD}
+ls
